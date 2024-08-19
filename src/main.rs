b625d8c6370d06dd4b44b45c2d538c9371ed9777
nicholaskoblitz <nicholask1@outlook.com>
2024-08-19 16:15:53 -0400

    Added more items to the TODO list

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ TODO: Create function to create a todo file in the user's documents folder
     - Find the location of the user folder
     - cd to Document folder
     - Create a folder called 'Todos'
+    - Let the user name the todos file
 TODO: Create function to write to Todo file
+    - Open the 'Todos' file with the name specified by the user
 TODO: Create function to read from Todo file
 TODO: Add the function to strikethrough completed Todo item
 TODO: Add the ability to create sub-lists
+TODO: Add a config.toml file
+    - Make the program cross-compile.
  */
 
 fn main() {
