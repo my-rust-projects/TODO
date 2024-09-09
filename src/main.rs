fe8af888963b9b42612fe6392556bb336c70280e
nicholaskoblitz <nicholask1@outlook.com>
2024-09-09 14:40:23 -0400

    Set up main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
 use todo::tool::tool::Tool;
+use todo;
 fn main() {
+    todo::check_os().unwrap();
+
     let args: Vec<String> = env::args().collect();
+    let tool = Tool::build(&args).unwrap();
 
-   let t = Tool::build(&args).unwrap();
-
-    println!("{:?}", t.args);
-
+    todo::run(tool).unwrap();
 }
